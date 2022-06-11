95f7b928531f2cb186f0aa458593e108e9287889
Slater McArdle <slater.mcardle@outlook.com>
2022-06-11 09:41:42 -0700

    updated image calls for contact section

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -10,10 +10,10 @@ function Contact() {
                 <hr/>
                 <Row>
                     <Col>
-                        <Button variant="secondary" href="https://github.com/WindWalker-92"><img src="/images/GitHub-Mark-32px.png" alt="github logo" wdth="20px" height="20px" style={{paddingRight: 5}}/>Github</Button>
+                        <Button variant="secondary" href="https://github.com/WindWalker-92"><img src={require("/images/GitHub-Mark-32px.png")} alt="github logo" wdth="20px" height="20px" style={{paddingRight: 5}}/>Github</Button>
                     </Col>
                     <Col>
-                        <Button variant="secondary" href="https://www.linkedin.com/in/slatermcardle/"><img src="/images/LI-In-Bug.png" alt="linkedin logo" width="24px" height="20px" style={{paddingRight: 2}}></img>Linkedin</Button>
+                        <Button variant="secondary" href="https://www.linkedin.com/in/slatermcardle/"><img src={require("/images/LI-In-Bug.png")} alt="linkedin logo" width="24px" height="20px" style={{paddingRight: 2}}></img>Linkedin</Button>
                     </Col>
                     <Col>
                         <Button variant="secondary" href="mailto:slater.mcardle@outlook.com"><Envelope width="20px" height="20px" style={{ paddingRight: 5, paddingBottom: 2}}/>Email Me</Button>
